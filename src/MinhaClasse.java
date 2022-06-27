f9f7c63f7daf159c2292ed7aeb58c171a36171f2
carolcastro84 <carolcastro84@gmail.com>
2022-06-26 23:43:28 -0300

    Exercicios 20220626

---
+++ src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Fui executado pelo terminal!");
+        //double byte short int long float boolean char tipos primitivos 
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println(smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println(smartTv.volume);
+    }
+}
